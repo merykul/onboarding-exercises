ae4fd796c662dd65c370abd24e76f580eabcf866
merykul <mariakulv@gmail.com>
2023-07-19 15:42:04 +0300

    adjusted according to the comments

--- hashes_and_arrays.rb
+++ hashes_and_arrays.rb
@@ -1,64 +1,57 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
+
 class Library
+  @books = []
+
   book1 = {
-    id: 1,
+    id: @books.length + 1,
     year: 1956,
     status: 'available',
     author: 'Lucy Maud Montgomery',
     name: 'Anne of Green Gables'
   }
+  @books << book1
 
   book2 = {
-    id: 2,
+    id: @books.length + 1,
     year: 1985,
     status: 'unavailable',
     author: 'Robin Norwood',
     name: 'Women Who Love Too Much'
   }
+  @books << book2
 
   book3 = {
-    id: 3,
+    id: @books.length + 1,
     year: 2020,
     status: 'available',
     author: 'Edith Eva Eger',
     name: 'The Gift'
   }
+  @books << book3
 
-  books = {
-    book1[:id] => book1,
-    book2[:id] => book2,
-    book3[:id] => book3
-  }
-
-  def find_book(books, search_variable)
-    search_result = books.values.select do |book|
-      [book[:author], book[:name], book[:id]].include?(search_variable)
-    end
-    puts search_result
+  def find_book(search_variable)
+    puts @books.any? { |book| book.values.include?(search_variable) }
   end
 
-  def update_book_status(books, id, new_status)
-    if books.key?(id)
-      books[id][:status] = new_status
-      puts "#{books[id][:name]} book new status is #{books[id][:status]}"
+  def update_book_status(id, new_status)
+    book = @books.find { |book| book[:id] == id }
+    if book
+      book[:status] = new_status
+      puts "#{book[:name]} book new status is #{book[:status]}"
     else
       puts 'Book is not found in the Library'
     end
   end
 
-  def acquire_new_book(books, id, author, year, status, name)
+  def acquire_new_book(author, year, status, name)
     book = {
-      id: id,
+      id: @books.length + 1,
       year: year,
       status: status,
       author: author,
       name: name
     }
-    books[id] = book
-
-    books.each do |id, book|
-      puts "Book ID: #{id}"
-      puts "Name: #{book[:name]}"
-    end
+    @books << book
   end
 end
