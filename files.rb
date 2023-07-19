c6c65033ec01b5af5fe1807e2a5298ae5f05c9cb
merykul <mariakulv@gmail.com>
2023-07-19 19:21:23 +0300

    created initial version of file_analyze method

--- files.rb
+++ files.rb
@@ -2,23 +2,25 @@
 class FileAnalyzer
   def initialize(path)
     @file = path
+    file_analyze
+  end
+
+  private
+
+  def file_analyze
+    return unless File.exist?(@file)
+
     count_empty_lines
     count_lines
     count_y_symbols
   end
 
-  private
-
   def count_empty_lines
-    if File.exist?(@file)
-      empty_lines = 0
-      File.foreach(@file) do |line|
-        empty_lines += 1 if line.strip.empty?
-      end
-      puts "Empty lines: #{empty_lines}"
-    else
-      puts "File with #{@file} path is not found. Please, check if you entered correct path."
+    empty_lines = 0
+    File.foreach(@file) do |line|
+      empty_lines += 1 if line.strip.empty?
     end
+    puts "Empty lines: #{empty_lines}"
   end
 
   def count_lines
