92db64169db31810b6981e46dcaf24e4484cbb87
merykul <mariakulv@gmail.com>
2023-07-20 12:59:31 +0300

    updated version of the task

--- script.rb
+++ script.rb
@@ -1,12 +1,9 @@
 command = ARGV.empty? ? (puts 'Please pass argument to the script') : ARGV[0]
 
-if command
-  result = system(command)
+result = system(command)
 
-  if result
-    puts 'Completed successfully'
-    puts command.to_s
-  else
-    puts 'Failed to run'
-  end
+if result
+  puts 'Completed successfully'
+else
+  puts 'Failed to run'
 end
